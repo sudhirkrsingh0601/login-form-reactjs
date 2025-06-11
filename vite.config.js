70aa66f79c94df2d89fd327e9ba11608bc8ec62b
sudhirkrsingh0601 <mr.sudhirkumarsingh8@gmail.com>
2025-06-11 18:00:03 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/login-form-reactjs/', // 👈 Replace with your GitHub repo name
+  base: 'login-form-reactjs', // 👈 Replace with your GitHub repo name
 })
